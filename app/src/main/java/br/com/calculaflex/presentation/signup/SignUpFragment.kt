54f0091db2327cad1dd90f89d947990ce7a80e17
Helton Isac <helton.isac@gmail.com>
2020-11-07 17:55:15 -0300

    Navigating to SignUpFragment.kt

--- app/src/main/java/br/com/calculaflex/presentation/signup/SignUpFragment.kt
+++ app/src/main/java/br/com/calculaflex/presentation/signup/SignUpFragment.kt
@@ -1,8 +1,25 @@
 package br.com.calculaflex.presentation.signup
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.navigation.fragment.NavHostFragment
 import br.com.calculaflex.R
 import br.com.calculaflex.presentation.base.BaseFragment
 
 class SignUpFragment : BaseFragment() {
     override val layout = R.layout.fragment_sign_up
+
+    private lateinit var tvTerms: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvTerms = view.findViewById(R.id.tvTerms)
+
+        tvTerms.setOnClickListener {
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_signUpFragment_to_termsFragment)
+        }
+    }
 }
