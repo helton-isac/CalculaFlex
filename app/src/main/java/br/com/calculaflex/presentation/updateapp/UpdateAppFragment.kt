ec592e3d1301cbd41df0e3e62008883ffb7ae5b1
Helton Isac <helton.isac@gmail.com>
2020-11-12 20:22:36 -0300

    Creating UpdateAppFragment

---
+++ app/src/main/java/br/com/calculaflex/presentation/updateapp/UpdateAppFragment.kt
@@ -0,0 +1,57 @@
+package br.com.calculaflex.presentation.updateapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import br.com.calculaflex.R
+
+class UpdateAppFragment : Fragment() {
+    private lateinit var btUpdateApp: Button
+    private lateinit var btUpdateLater: Button
+    override fun onViewCreated(
+        view: View, savedInstanceState:
+        Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpView(view)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_update_app, container, false)
+    }
+
+    private fun setUpView(view: View) {
+        btUpdateApp = view.findViewById(R.id.btUpdateApp)
+        btUpdateLater = view.findViewById(R.id.btUpdateLater)
+        btUpdateApp.setOnClickListener {
+            openAppInStore()
+        }
+        btUpdateLater.setOnClickListener {
+            activity?.finish()
+        }
+    }
+
+    private fun openAppInStore() {
+        var intent: Intent
+        val packageName = activity?.packageName
+        try {
+            intent = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id= $packageName"))
+            startActivity(intent)
+        } catch (e: android.content.ActivityNotFoundException) {
+            intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id= $packageName")
+            )
+            startActivity(intent)
+        }
+    }
+}
