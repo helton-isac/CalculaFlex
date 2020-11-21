e036811f29b5e182598feb8518ef57560195b814
Helton Isac <helton.isac@gmail.com>
2020-11-20 22:42:52 -0300

    Flavor indicator

--- app/src/main/java/br/com/calculaflex/presentation/betterfuel/BetterFuelFragment.kt
+++ app/src/main/java/br/com/calculaflex/presentation/betterfuel/BetterFuelFragment.kt
@@ -1,6 +1,5 @@
 package br.com.calculaflex.presentation.betterfuel
 
-import CustomDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -24,6 +23,7 @@ import br.com.calculaflex.extensions.getDouble
 import br.com.calculaflex.extensions.getString
 import br.com.calculaflex.presentation.base.auth.BaseAuthFragment
 import br.com.calculaflex.presentation.watchers.DecimalTextWatcher
+import br.com.calculaflexlib.components.customdialog.CustomDialog
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
